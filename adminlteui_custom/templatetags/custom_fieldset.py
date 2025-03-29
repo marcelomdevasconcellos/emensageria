e8dd5a4dc19b3b0d5b4d8fcdf7ec791f6d5244a1
Marcelo Vasconcellos <marcelomdevasconcellos@gmail.com>
2025-03-29 09:05:15 -0400

    Fix flake8

--- adminlteui_custom/templatetags/custom_fieldset.py
+++ adminlteui_custom/templatetags/custom_fieldset.py
@@ -16,16 +16,18 @@ def adminlte_paginator_number(cl, i):
     Generate an individual page index link in a paginated list.
     """
     if i == DOT or i == '…':
-        # <li class="paginate_button active"><a href="#" aria-controls="example2" data-dt-idx="3" tabindex="0">{}</a></li>'
         return format_html(
-            '<li class="paginate_button"><a href="javascript:void(0);" aria-controls="example2" data-dt-idx="3" tabindex="0">… </a></li>')
+            '<li class="paginate_button"><a href="javascript:void(0);" '
+            'aria-controls="example2" data-dt-idx="3" tabindex="0">… </a></li>')
     elif i == cl.page_num:
         return format_html(
-            '<li class="paginate_button active"><a href="javascript:void(0);" aria-controls="example2" data-dt-idx="3" tabindex="0">{}</a></li>',
+            '<li class="paginate_button active"><a href="javascript:void(0);" '
+            'aria-controls="example2" data-dt-idx="3" tabindex="0">{}</a></li>',
             i)
     else:
         return format_html(
-            '<li class="paginate_button "><a href="{}" {} aria-controls="example2" data-dt-idx="3" tabindex="0">{}</a></li>',
+            '<li class="paginate_button "><a href="{}" {} aria-controls="example2" '
+            'data-dt-idx="3" tabindex="0">{}</a></li>',
             cl.get_query_string({PAGE_VAR: i}),
             mark_safe(
                 ' class="end"' if i == cl.paginator.num_pages - 1 else ''),
