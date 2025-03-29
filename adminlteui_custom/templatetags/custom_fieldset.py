3350dd83245f2f1d5648a89ca699982bbbb46557
Marcelo Vasconcellos <marcelomdevasconcellos@gmail.com>
2025-03-29 08:39:41 -0400

    Correção de paginação e reconsulta de lote com erro 301

--- adminlteui_custom/templatetags/custom_fieldset.py
+++ adminlteui_custom/templatetags/custom_fieldset.py
@@ -1,12 +1,40 @@
 from django import template
+from django.contrib.admin.views.main import (
+    PAGE_VAR, )
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 from django.utils.text import slugify
 
 register = template.Library()
 
+DOT = '.'
+
+
+@register.simple_tag
+def adminlte_paginator_number(cl, i):
+    """
+    Generate an individual page index link in a paginated list.
+    """
+    if i == DOT or i == '…':
+        # <li class="paginate_button active"><a href="#" aria-controls="example2" data-dt-idx="3" tabindex="0">{}</a></li>'
+        return format_html(
+            '<li class="paginate_button"><a href="javascript:void(0);" aria-controls="example2" data-dt-idx="3" tabindex="0">… </a></li>')
+    elif i == cl.page_num:
+        return format_html(
+            '<li class="paginate_button active"><a href="javascript:void(0);" aria-controls="example2" data-dt-idx="3" tabindex="0">{}</a></li>',
+            i)
+    else:
+        return format_html(
+            '<li class="paginate_button "><a href="{}" {} aria-controls="example2" data-dt-idx="3" tabindex="0">{}</a></li>',
+            cl.get_query_string({PAGE_VAR: i}),
+            mark_safe(
+                ' class="end"' if i == cl.paginator.num_pages - 1 else ''),
+            i,
+        )
+
 
 @register.filter('div_name')
-def div_name(
-        name):
+def div_name(name):
     if name:
         name = name.replace(' ', '_')
         return name.lower() + '_div'
@@ -15,49 +43,43 @@ def div_name(
 
 
 @register.filter('not_none')
-def not_none(
-        obj):
+def not_none(obj):
     if obj is None:
         return "-"
     return obj
 
 
 @register.filter('test')
-def test(
-        obj):
+def test(obj):
     try:
         print(obj.__dict__)
-    except Exception as e:
-        print('erro:', e)
+    except Exception as exc:
+        print(exc)
     return ''
 
 
 @register.filter('tab_organize')
-def tab_organize(
-        modelName):
+def tab_organize(modelName):
     try:
         if modelName.tab:
             return False
-    except Exception as e:
-        print('erro:', e)
+    except Exception as exc:
+        print(exc)
         return True
 
 
 @register.filter('to_slug')
-def to_slug(
-        txt):
+def to_slug(txt):
     return slugify(txt)
 
 
 @register.filter('to_str')
-def to_str(
-        int):
+def to_str(int):
     return str(int)
 
 
 @register.filter('bootstrap_cols_number')
-def bootstrap_cols_number(
-        field):
+def bootstrap_cols_number(field):
     # Example: <div class="col-md-{{ field|bootstrap_cols_number }}">
     COLUMN_DEFAULT = 4
     if not field.is_readonly:
--- apps/esocial/management/commands/esocial_consultar.py
+++ apps/esocial/management/commands/esocial_consultar.py
@@ -1,4 +1,6 @@
 from django.core.management.base import BaseCommand
+from django.db.models import Q
+
 from ...choices import STATUS_TRANSMISSOR_ENVIADO
 from ...models import Lotes
 
@@ -7,7 +9,8 @@ class Command(BaseCommand):
     help = 'Consultar eSocial'
 
     def handle(self, *args, **options):
-        trm = Lotes.objects.filter(status=STATUS_TRANSMISSOR_ENVIADO)
+        trm = Lotes.objects.filter(
+            Q(status=STATUS_TRANSMISSOR_ENVIADO) | Q(retorno_consulta__icontains='Código do erro: 301'))
         for tle in trm:
             tle.consultar()
             self.stdout.write('\n{} {} {} {}'.format(
